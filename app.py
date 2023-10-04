7dc7f1249c020d967aa57226bdca3ffb5f2ef216
belva andini <065002200035@std.trisakti.ac.id>
2023-10-04 11:25:40 +0700

    upload all files

---
+++ app.py
@@ -0,0 +1,45 @@
+import streamlit as st
+import pandas as pd
+import matplotlib.pyplot as plt
+
+# Judul aplikasi
+st.title('Streamlit Simple App')
+
+# Menambahkan navigasi di sidebar
+page = st.sidebar.radio("Pilih halaman", ["Dataset", "Visualisasi"])
+
+if page == "Dataset":
+    st.header("Halaman Dataset")
+    
+    # Baca file CSV
+    data = pd.read_csv("pddikti_example.csv")
+
+    # Tampilkan data di Streamlit
+    st.write(data)
+
+elif page == "Visualisasi":
+    st.header("Halaman Visualisasi")
+    st.set_option('deprecation.showPyplotGlobalUse', False)
+    
+    # Baca file CSV
+    data = pd.read_csv("pddikti_example.csv")
+    
+    # Filter berdasarkan universitas
+    selected_universitas = st.selectbox('Pilih Universitas', data['universitas'].unique())
+    filtered_data = data[data['universitas'] == selected_universitas]
+    
+    # Buat visualisasi
+    plt.figure(figsize=(12, 6))
+    for prog_studi in filtered_data['program_studi'].unique():
+        subset = filtered_data[filtered_data['program_studi'] == prog_studi]
+        
+        # Urutkan data berdasarkan 'id' dengan urutan menurun
+        subset = subset.sort_values(by="id", ascending=False)
+        plt.plot(subset['semester'], subset['jumlah'], label=prog_studi)
+
+    plt.xlabel('Semester')
+    plt.title(f"Visualisasi Data untuk {selected_universitas}")
+    plt.xticks(rotation=90)  # Rotasi label sumbu x menjadi vertikal
+    plt.ylabel('Jumlah')
+    plt.legend()
+    st.pyplot()
